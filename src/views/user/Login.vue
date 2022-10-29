0fb8995c29f1105a58b213fbb490335dfb4350a2
AchmadSu <ecepentis@gmail.com>
2022-10-29 15:26:13 +0700

    layouting login

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -18,25 +18,34 @@
                 <img src="src/assets/img/Data_security_28.jpg" class="img-fluid" alt="...">
             </div>
             <div class="col-md-6 col-sm-12 px-lg-5 text-center">
-                <form class="form" action="#" method="POST">
+                <form class="form" action="#" id="app" @submit.prevent="onSubmit" method="POST" novalidate="true">
                     <div class="input-group mb-3 py-sm-3 py-md-0 py-lg-1">
                         <h3 class="fw-bolder">
                             Log In
                         </h3>
                     </div>
                     <div class="py-lg-4 py-md-0 py-sm-1">
-                        <div class="input-group mb-3">
+                        <div class="input-group mb-3" :class="{ error: v$.form.email.$errors.length }">
                             <span class="input-group-text bg-transparent" id="basic-addon1">
                                 <font-awesome-icon class="text-secondary" icon="fa-solid fa-envelope" />
                             </span>
-                            <input name="email" type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1">
+                            <input 
+                                name="email" type="email" class="form-control"
+                                placeholder="Email" aria-label="Email" 
+                                aria-describedby="basic-addon1"
+                                v-model="email" required
+                            />
                         </div>
                         <div v-if="passwordHidden">
-                            <div class="input-group mb-3">
+                            <div class="input-group mb-3" :class="{ error: v$.form.email.$errors.length }">
                                 <span class="input-group-text bg-transparent" id="basic-addon1">
                                     <font-awesome-icon class="text-secondary" icon="fa-solid fa-lock" />
                                 </span>
-                                <input name="password" type="password" class="form-control" v-model="passwordText" placeholder="Password" aria-label="Password" aria-describedby="basic-addon2" />
+                                <input 
+                                    name="password" type="password" class="form-control"
+                                    v-model="passwordText" placeholder="Password" aria-label="Password"
+                                    aria-describedby="basic-addon2" required minlength="6" 
+                                    />
                                 <button @click="showPassword" class="btn btn-outline-secondary" id="button-addon2"><font-awesome-icon icon="fa-solid fa-eye" /></button>
                             </div>
                         </div>
@@ -45,33 +54,78 @@
                                 <span class="input-group-text bg-transparent" id="basic-addon1">
                                     <font-awesome-icon class="text-secondary" icon="fa-solid fa-lock" />
                                 </span>
-                                <input name="password" type="text" class="form-control" v-model="passwordText" placeholder="Password" aria-label="Password" aria-describedby="basic-addon2" />
+                                <input 
+                                    name="password" type="text" class="form-control"
+                                    v-model="passwordText" placeholder="Password"
+                                    aria-label="Password" aria-describedby="basic-addon2"
+                                    required minlength="6"
+                                />
                                 <button @click="hidePassword" class="btn btn-outline-secondary" id="button-addon2"><font-awesome-icon icon="fa-solid fa-eye-slash" /></button>
                             </div>
                         </div>
                         <button type="submit" class="btn btn-primary" style="width:100%;">Masuk</button>
                     </div>
                 </form>
-                <p class="mt-5 mt-md-3 mt-lg-1">Belum menjadi anggota? Daftar di <a href="#">sini</a></p>
+                <div :class="windowWidth >= 760 ? 'row my-md-3 my-lg-0' : 'd-none'">
+                    <div class="col-6 text-right">
+                        <p>Belum memiliki akun?</p>
+                    </div>
+                    <div class="col-6">
+                        <button class="btn btn-success w-100">Daftar</button>
+                    </div>
+                </div>
+                <div :class="windowWidth < 760 ? 'row my-3' : 'd-none'">
+                    <div class="col-12">
+                        <p>Atau</p>
+                    </div>
+                    <div class="col-12">
+                        <button class="btn btn-success w-100">Daftar</button>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row text-center">
+        <div class="row text-center py-3">
             <p class="text-secondary">Eraksa <font-awesome-icon icon="fa-solid fa-copyright" /> 2023</p>
         </div>
     </div>
 </template>
 
 <script>
+    
+    import useVuelidate from '@vuelidate/core'
+    import { required, email, minLength } from '@vuelidate/validators'
     export default {
-        data () {
+        setup(){
+            return { v$: useVuelidate() }
+        },
+        data (){
             return {
                 passwordHidden: {
                     default: true,
                     type: Boolean
                 },
-                windowWidth: window.innerWidth
+                windowWidth: window.innerWidth,
+                form : {
+                    email : '',
+                    passwordText : '',
+                },
             }
         },
+
+        validations(){
+            return {
+                form : {
+                    email : {
+                        required, email
+                    },
+                    passwordText : {
+                        required,
+                        min: minLength(6)
+                    }
+                }
+            }
+        },
+
         methods: {
             hidePassword() {
                 this.passwordHidden = true;
