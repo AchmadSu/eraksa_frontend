582e71cd10dd3d4e718434e8b74b05003020a8c4
AchmadSu <ecepentis@gmail.com>
2022-11-18 16:09:47 +0700

    add some fontawesome and add global width window

--- src/main.js
+++ src/main.js
@@ -11,11 +11,14 @@ import axios from 'axios'
 axios.defaults.withCredentials = true   
 axios.defaults.baseURL = 'http://127.0.0.1:8000/api'
 axios.defaults.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+// axios.defaults.headers['Set-Cookie'] = document.cookie;
 axios.defaults.headers['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = "*";
+// axios.defaults.headers.common['Access-Control-Expose-Headers'] = "Access-Token, Uid";
 axios.defaults.headers['Access-Control-Allow-Methods'] = "DELETE, PUT, GET, POST";
 axios.defaults.headers['Access-Control-Allow-Headers'] = "Content-type, Authorization, X-Requested-With";
 axios.defaults.withCredentials = true;
+
 
 // import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -26,6 +29,7 @@ import './assets/fontawesome-free-6.2.0-web/css/fontawesome.css'
 import './assets/fontawesome-free-6.2.0-web/css/fontawesome.min.css'
 import './assets/font-awesome-4.7.0/css/font-awesome.min.css'
 import './assets/img/login.jpg'
+
 // @import = url('https://fonts.googleapis.com/css?family=Roboto+Condensed');
 // import { library } from '@fortawesome/fontawesome-svg-core'
 
@@ -41,17 +45,27 @@ import {
     faPhone, 
     faTriangleExclamation,
     faCircleCheck,
+    faArrowLeft,
+    faUserPlus,
+    faPaperPlane,
+    faRightToBracket
 } from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
+import VueCryptojs from 'vue-cryptojs'
 
 // createApp(App).mount('#app')
 library.add(
     faUser, faEnvelope, faKey, faLock, faCopyright, faEye, faEyeSlash, faPhone, faTriangleExclamation,
-    faCircleCheck
+    faCircleCheck, faArrowLeft, faUserPlus, faPaperPlane, faRightToBracket
 )
 // Vue.use(ModalPlugin)
 const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon)
+app.config.globalProperties.$baseUrl = window.location.origin
+// app.config.globalProperties.$windowWidth = window.innerWidth
+app.config.globalProperties.$widthRotatePhone = 760
+app.config.globalProperties.$widthLandscapePhone = 992
+app.config.globalProperties.$widthComputer = 1200
 app.use(router)
 // library.add(faUser)
 // Vue.config.productionTip = false
