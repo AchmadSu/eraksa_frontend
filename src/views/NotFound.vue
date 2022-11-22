e96bfe05d3413a160e67fdb5ae1dac7edef87248
AchmadSu <ecepentis@gmail.com>
2022-11-22 19:28:47 +0700

    add not found page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="min-vh-100 container-fluid text-center bg-light text-danger position-absolute top-50 start-50 translate-middle p-5">
+        <div class="my-5">
+            <h3><font-awesome-icon icon="fa-solid fa-triangle-exclamation" /></h3>
+            <h4>404 | Page Not Found!</h4>
+        </div> 
+    </div>
+</template>
+
+<script>
+// import LoadingPage from './components/LoadingPage';
+export default {
+  // name: 'App',
+  data(){
+    return {
+    //   isLoading: true,
+      windowWidth: window.innerWidth,
+      currentYear: new Date().getFullYear(),
+    }
+  },
+  mounted(){
+    // this.isLoading = true;
+    window.onresize = () => {
+        this.windowWidth = window.innerWidth
+    }
+    // window.scrollTo(0,0);
+    setTimeout(() => this.isLoading = false, 2000);
+  }
+}
+
+</script>
