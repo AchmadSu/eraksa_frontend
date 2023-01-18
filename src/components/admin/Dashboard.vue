447686ee86f2505eebfc1b650a3427a2e650e2fe
AchmadSu <ecepentis@gmail.com>
2023-01-18 22:19:19 +0700

    configure layout dashboard

--- src/components/admin/Dashboard.vue
+++ src/components/admin/Dashboard.vue
@@ -3,7 +3,7 @@
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
         <a href="#" style="text-decoration: none;" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
-            <i class="fa fa-download fa-sm text-white-50"></i> Generate Report
+            <i class="fa fa-arrow-down"></i>&nbsp;Unduh Laporan 
         </a>
     </div>
 
@@ -207,6 +207,11 @@
                         </span>
                     </div>
                 </div>
+            </div>
+            <div class="col-12 mb-5">
+                <a href="#" style="text-decoration: none;" :class="this.windowWidth <= this.$widthPotraitPhone ? 'w-100 d-md-none btn btn-sm btn-primary shadow-sm' : 'd-none'">
+                    <i class="fa fa-arrow-down"></i>&nbsp;Unduh Laporan
+                </a>
             </div>
         </div>
     </div>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -35,18 +35,28 @@
                             <Dashboard></Dashboard>
                         </div>
                         <!-- Page Heading -->
-                        <div v-if="this.errorLoans == true" class="text-center row col-12 d-sm-flex align-items-center justify-content-between mb-4">
-                            <h1 class="h3 mb-0 text-gray-800">Tidak ada Aset yang dapat dipinjam untuk saat ini</h1>
+                        <div v-if="this.errorLoans == true" class="text-lg-center text-sm-justify row col-12 d-sm-flex align-items-center justify-content-center mb-3">
+                            <h3 class="h4 mb-0 text-gray-800">Tidak ada Aset yang dapat dipinjam untuk saat ini</h3>
                         </div>
                         <div v-else class="d-sm-flex align-items-center justify-content-between mb-4">
-                            <h1 class="h3 mb-0 text-gray-800">Aset yang dapat dipinjam</h1>
+                            <h1 class="h4 mb-0 text-gray-800">Aset yang dapat dipinjam</h1>
+                            <a href="#" style="text-decoration: none;" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+                                <i class="fa fa-paper-plane"></i>&ensp;Ajukan Peminjaman
+                            </a>
                         </div>
                         <div class="row" v-if="this.errorLoans == true">
-                            <div class="col-3"></div>
-                            <div class="col-4 mx-5">
+                            <div v-if="this.windowWidth >= this.$widthLandscapePhone" class="col-3">&nbsp;
+                            </div>
+                            <div v-if="this.windowWidth >= this.$widthLandscapePhone" class="col-4 mx-5">
                                 <img class="mx-5 w-100 img-thumbnails" :src="this.$baseUrl+'/src/assets/img/404.png'" alt="">
                             </div>
-                            <h6 class="text-center my-3">Atau koneksi anda terganggu, silakan muat ulang!</h6>
+                            <div v-else-if="this.windowWidth >= this.$widthPotraitPhone" class="col-11 mx-5">
+                                <img class="w-75 mx-5 px-5 img-thumbnails" :src="this.$baseUrl+'/src/assets/img/404.png'" alt="">
+                            </div>
+                            <div v-else class="col-12">
+                                <img  class="w-100 img-thumbnails" :src="this.$baseUrl+'/src/assets/img/404.png'" alt="">
+                            </div>
+                            <h6 class="text-center my-3">Atau mungkin koneksi anda terganggu, silakan muat ulang!</h6>
                         </div>
                         <div class="row">
                             <div v-for="item in this.loansArray" :key="item.id" class="col-sm-6 col-lg-4">
@@ -67,13 +77,18 @@
                                         <div class=" mt-3">
                                             <div class="mt-3"> 
                                                 <span class="text1">XXX kali Dipinjam <br>
-                                                <span v-if="this.windowWidth <= this.$widthPotraitPhone" class="text2">Ketuk untuk selengkapnya</span>
+                                                <span v-if="this.windowWidth <= this.$widthLandscapePhone" class="text2">Ketuk untuk selengkapnya</span>
                                                 <span v-else class="text2">Klik untuk selengkapnya</span>
                                             </span>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                            <div class="col-12">
+                                <a href="#" style="text-decoration: none;" :class="this.windowWidth <= this.$widthPotraitPhone ? 'w-100 d-md-none btn btn-sm btn-primary shadow-sm' : 'd-none'">
+                                    <i class="fa fa-paper-plane"></i>&ensp;Ajukan Peminjaman
+                                </a>
                             </div>
                         </div>
                     </div>
